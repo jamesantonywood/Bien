79e5de34ac2bdc4b3dcadff5c20f2d81133316f3
jamesantonywood <james@curious12.com>
2022-02-18 22:39:37 +0000

    Added ambience and phone number to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_205902) do
+ActiveRecord::Schema.define(version: 2022_02_18_222525) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_02_13_205902) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "ambience"
   end
 
 end
